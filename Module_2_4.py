0140613c32b8be2cb2d1174432ac6227ad87b4b4
SneKKaY <120265382+SneKKaY@users.noreply.github.com>
2024-11-23 15:16:03 +0300

    Add files via upload

---
+++ Module_2_4.py
@@ -0,0 +1,30 @@
+# for i in range(1, 11):
+#     for k in range(1, 11):
+#         print(f'{i} * {k} = {i*k}')
+#
+# dict_ = {'Dima' : 21, 'Vany' : 18, 'Sofia' : 9}
+# for i,k in dict_.items():
+#     print(i,k)
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    if i == 1:
+        continue
+
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+
+print('Primes:', primes)
+
+print('Not Primes:', not_primes)
